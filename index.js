e68acbb11e527403d727266131883c5b57aff91e
ali tarichi <tarichiali@gmail.com>
2025-02-10 16:26:48 +0300

    added post func

--- index.js
+++ index.js
@@ -22,8 +22,52 @@ const Exercise = mongoose.model("Exercise", ExerciseSchema);
 
 app.use(cors());
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/api/users", async (req, res) => {
+  const userObj = new User({
+    username: req.body.username,
+  });
+  try {
+    const user = await userObj.save();
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/api/users/:_id/exercises", async (req, res) => {
+  const id = req.params._id;
+  const { description, duration, date } = req.body;
+
+  try {
+    const user = await User.findById(id);
+    if (!user) {
+      res.send("Could not find user");
+    } else {
+      const exerciseObj = new Exercise({
+        user_id: user._id,
+        description,
+        duration,
+        date: date ? new Date() : new Date(),
+      });
+      const exercise = await exerciseObj.save();
+      res.json({
+        _id: user._id,
+        username: user.username,
+        description: exercise.description,
+        duration: exercise.duration,
+        date: new Date(exercise.date).toDateString(),
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.send("There was an error saving this exercise");
+  }
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
