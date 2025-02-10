96d0c0af86461e96555dcaf4e4a24adc199cbe41
ali tarichi <tarichiali@gmail.com>
2025-02-10 15:53:50 +0300

    mongoose init

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+mongoose.connect(process.env.MONGO_URI);
+
+const UserSchema = new Schema({
+  username: String,
+});
+const User = mongoose.model("User", UserSchema);
+
+const ExerciseSchema = new Schema({
+  user_id: { type: String, required: true },
+  description: String,
+  duration: Number,
+  date: Date,
+});
+const Exercise = mongoose.model("Exercise", ExerciseSchema);
 
 app.use(cors());
 app.use(express.static("public"));
